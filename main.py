8f5996a404f4f1d54de1a359fbcc2c375cb46ab5
Олег Вишняков <oleg.vishnyakov.02@inbox.ru>
2024-07-04 21:20:32 +0300

    added the output of all results

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
 target = 200
+results = []
 
-def find(answer, array, str):
+def find(answer, array, line):
     if not array:
         if (answer == target):
-            return str
+            if line not in results:
+                results.append(line)
         return None
 
     number = array[0]
     array2 = array[1:]
 
     # subtraction
-    if str:
-        str_add = f"{str}-{number}"
+    if line:
+        str_add = f"{line}-{number}"
     else:
         str_add = f"{number}"
     result = find(answer - number, array2, str_add)
-    if result:
-        return result
+
 
     # addition
-    if str:
-        str_add = f"{str}+{number}"
+    if line:
+        str_add = f"{line}+{number}"
     else:
         str_add = f"{number}"
     result = find(answer + number, array2, str_add)
-    if result:
-        return result
 
     # empty
-    if str:
-        str_add = f"{str}{number}"
+    if line:
+        str_add = f"{line}{number}"
         answer2 = eval(str_add)
     else:
         str_add = f"{number}"
         answer2 = number
     result = find(answer2, array2, str_add)
-    if result:
-        return result
 
-    return None
+
+    return results
 
 def main():
     array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    str = ""
+    line = ""
     answer = 0
-    result = find(answer, array, str)
+    result = find(answer, array, line)
 
     if result:
-        print(f"Результат: {result} = {target}")
+        print("Результаты: ")
+        for i in result:
+            print(i)
     else:
         print("Не найдено такого примера")
 
